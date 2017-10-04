b88ea7636f939a7cc1e41f973ea9363de3f71186
Brian <mayrose.dg@gmail.com>
2017-10-03 23:06:37 -0700

    basic skeleton

---
+++ AddressBook/WebContent/javascript/addressbook.js
@@ -0,0 +1,16 @@
+/* Module for Address Book application */
+var AddressBook = function () {
+
+  /* add members here */
+
+  var init = function () {
+    /* add code to initialize this module */
+  };
+
+  /* execute the init function when the DOM is ready */
+  $(init);
+
+  return {
+    /* add members that will be exposed publicly */
+  };
+}();
